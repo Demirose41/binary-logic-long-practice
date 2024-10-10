ede9626aaf71d074165e327427ab41c922162cbe
demirose41 <demirose41@gmail.com>
2024-10-10 00:14:25 -0700

    1: completed convertToBase10

--- 1-base-conversions/1-convertToBase10.js
+++ 1-base-conversions/1-convertToBase10.js
@@ -1,14 +1,29 @@
 // Convert the integers in the console.logs below to base 10:
+const addZeros = require('../utils/addZeros');
 
 /******************************************************************************/
 
 const convertToBase10 = str => {
-  // Your code here
+  let marker = str.slice(1,2);
+  let string = str.slice(2);
+  if(string.length != 8){
+    string = addZeros(string, 8);
+  }
+  // Check if str in a binary
+  if(marker === 'b'){
+    let integers = string.split('').map((char)=> parseInt(char));
+    
+    return integers.reverse().reduce((accumulator, element, idx) => accumulator += (element * (2**idx)), 0);
+  }else{
+    let integers = string.split('').map((char)=> parseInt(char, 16));
+
+    return integers.reverse().reduce((accumulator, element, idx) => accumulator += (element * (16**idx)), 0);
+  }
 };
 
 /******************************************************************************/
 
-console.log(convertToBase10('0b1100')); // 12
+console.log(convertToBase10('8')); // 12
 console.log(convertToBase10('0b0101')); // 5
 console.log(convertToBase10('0b1000')); // 8
 console.log(convertToBase10('0b0111')); // 7
