8537f29e91fbfa94e3f600a23c0c9944326b16f0
demirose41 <demirose41@gmail.com>
2024-10-10 00:49:43 -0700

    completed convertToBase2

--- 1-base-conversions/1-convertToBase10.js
+++ 1-base-conversions/1-convertToBase10.js
@@ -12,33 +12,37 @@ const convertToBase10 = str => {
   // Check if str in a binary
   if(marker === 'b'){
     let integers = string.split('').map((char)=> parseInt(char));
-    
+
     return integers.reverse().reduce((accumulator, element, idx) => accumulator += (element * (2**idx)), 0);
-  }else{
+  }if(marker === 'x'){
     let integers = string.split('').map((char)=> parseInt(char, 16));
 
     return integers.reverse().reduce((accumulator, element, idx) => accumulator += (element * (16**idx)), 0);
+  }else{
+    return parseInt(str);
   }
 };
 
 /******************************************************************************/
 
-console.log(convertToBase10('8')); // 12
-console.log(convertToBase10('0b0101')); // 5
-console.log(convertToBase10('0b1000')); // 8
-console.log(convertToBase10('0b0111')); // 7
+// console.log(convertToBase10('8')); // 12
+// console.log(convertToBase10('0b0101')); // 5
+// console.log(convertToBase10('0b1000')); // 8
+// console.log(convertToBase10('0b0111')); // 7
 
-console.log('––––––');
+// console.log('––––––');
 
-console.log(convertToBase10('0b10100101')); // 165
-console.log(convertToBase10('0b11111111')); // 255
-console.log(convertToBase10('0b01010101')); // 85
-console.log(convertToBase10('0b00110011')); // 51
+// console.log(convertToBase10('0b10100101')); // 165
+// console.log(convertToBase10('0b11111111')); // 255
+// console.log(convertToBase10('0b01010101')); // 85
+// console.log(convertToBase10('0b00110011')); // 51
 
-console.log('––––––');
+// console.log('––––––');
 
-console.log(convertToBase10('0xf')); // 15
-console.log(convertToBase10('0xfa')); // 250
-console.log(convertToBase10('0x1234')); // 4660
-console.log(convertToBase10('0xc9a1')); // 51617
-console.log(convertToBase10('0xbf12')); // 48914
+// console.log(convertToBase10('0xf')); // 15
+// console.log(convertToBase10('0xfa')); // 250
+// console.log(convertToBase10('0x1234')); // 4660
+// console.log(convertToBase10('0xc9a1')); // 51617
+// console.log(convertToBase10('0xbf12')); // 48914
+
+module.exports = convertToBase10;
--- 1-base-conversions/2-convertToBase2.js
+++ 1-base-conversions/2-convertToBase2.js
@@ -1,23 +1,47 @@
 // Convert the integers in the console.logs below to base 2
+const convertToBase10 = require('./1-convertToBase10');
 
 /******************************************************************************/
 
 const convertToBase2 = element => {
-  // Your code here
+if(typeof element === 'string' ){
+  element = convertToBase10(element);  
+}
+let exponent = 0;
+let exponents = []; 
+let binary = '0b';
+while((powerOf2(exponent) <= element)){
+  exponents = [...exponents, exponent]
+  exponent++;
+}
+exponents.reverse().map((exponent) => {
+  if(element - powerOf2(exponent) >= 0){
+    binary += '1';
+    element -= powerOf2(exponent);
+  }else{
+    binary += '0';
+  } 
+})
+
+return binary;
 };
+
+function powerOf2(exponent){
+  return (1* (2**exponent))
+}
 
 /******************************************************************************/
 
-console.log(convertToBase2(4)); // 0b100
-console.log(convertToBase2(65)); // 0b1000001
-console.log(convertToBase2(256)); // 0b100000000
-console.log(convertToBase2(123)); // 0b1111011
-console.log(convertToBase2(1000)); // 0b1111101000
+console.log(convertToBase2(4) == 0b100)
+console.log(convertToBase2(65) == 0b1000001)
+console.log(convertToBase2(256) == 0b100000000)
+console.log(convertToBase2(123) == 0b1111011)
+console.log(convertToBase2(1000) == 0b1111101000)
 
 console.log('––––––');
 
-console.log(convertToBase2('0xf')); // 0b1111
-console.log(convertToBase2('0xfa')); // 0b11111010
-console.log(convertToBase2('0x1234')); // 0b1001000110100
-console.log(convertToBase2('0xc9a1')); // 0b1100100110100001
-console.log(convertToBase2('0xbf12')); // 0b1011111100010010
+console.log(convertToBase2('0xf') == 0b1111)
+console.log(convertToBase2('0xfa') == 0b11111010)
+console.log(convertToBase2('0x1234') == 0b1001000110100)
+console.log(convertToBase2('0xc9a1') == 0b1100100110100001)
+console.log(convertToBase2('0xbf12') == 0b1011111100010010)
